12ca9b79a76784bb7c02873fd27cf51cee6f9fab
Robert DeVore <deviodigital@gmail.com>
2018-01-12 03:46:00 -0500

    Added files for widgets, shortcodes & metaboxes

---
+++ admin/class-wpd-gear-metaboxes.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * The metabox functionality of the plugin.
+ *
+ * @link       https://www.wpdispensary.com/
+ * @since      1.0.0
+ *
+ * @package    WPD_Gear
+ * @subpackage WPD_Gear/admin
+ */
