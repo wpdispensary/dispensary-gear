1605d1706734adf659498ae27c4f7b43d794b2dd
Robert DeVore <deviodigital@gmail.com>
2019-12-10 16:03:26 -0500

    :ok_hand: IMPROVE: General code cleanup

--- public/class-wpd-gear-public.php
+++ public/class-wpd-gear-public.php
@@ -50,7 +50,7 @@ class WPD_Gear_Public {
 	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
-		$this->version = $version;
+		$this->version     = $version;
 
 	}
 
